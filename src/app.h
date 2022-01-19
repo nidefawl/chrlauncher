65f269535de7fe5824fc773ab0ba19b147480e54
Michael Hept <michael.hept@gmail.com>
2022-01-20 00:06:40 +0100

    Rename App and update License

--- src/app.h
+++ src/app.h
@@ -1,11 +1,13 @@
-// chrlauncher
+// chromiumlauncher
+// forked from chrlauncher
 // Copyright (c) 2015-2021 Henry++
+// Copyright (c) 2022 nidefawl
 
 #pragma once
 
-#define APP_NAME L"chrlauncher"
-#define APP_NAME_SHORT L"chrlauncher"
-#define APP_AUTHOR L"Henry++"
-#define APP_VERSION L"2.6"
-#define APP_VERSION_RES 2,6,0,0
-#define APP_COPYRIGHT L"(c) 2015-2021 " APP_AUTHOR L". All Rights Reversed."
+#define APP_NAME L"Chromium Launcher"
+#define APP_NAME_SHORT L"chromiumlauncher"
+#define APP_AUTHOR L"nidefawl (original by Henry++)"
+#define APP_VERSION L"2.7"
+#define APP_VERSION_RES 2,7,0,0
+#define APP_COPYRIGHT L"(c) 2022 nidefawl"
